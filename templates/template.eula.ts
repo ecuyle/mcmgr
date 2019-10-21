5d0e28e7699ea12fe8dd1412c74251e2e3eeae2b
Eric <ecuyle@gmail.com>
2019-10-21 01:42:59 -0700

    Refactors _createEulaWithUserInput to use array of values instead of .txt file

---
+++ templates/template.eula.ts
@@ -0,0 +1,5 @@
+export const DEFAULT_EULA_ROWS: Array<string> = [
+    '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).',
+    '#<insert date here>',
+    'eula=false',
+];
