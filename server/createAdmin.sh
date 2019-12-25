c625ea10aa659d53f387e3506cfedae89eddf5b4
Eric <ecuyle@gmail.com>
2019-12-24 22:52:07 -0800

    Fixes issues with logging in through cli created user

--- server/createAdmin.sh
+++ server/createAdmin.sh
@@ -3,5 +3,6 @@
 # hash password here and then send it over to be stored. we don't
 # want the password being stored in plaintext in the shell's logs
 read -p "Enter an admin username: " ADMIN_USERNAME
-RESULT=$(htpasswd -nBC 10 $ADMIN_USERNAME)
-node ./createAdmin.js $RESULT
+read -sp "Enter an admin password: " ADMIN_PASSWORD
+# RESULT=$(htpasswd -nBC 10 $ADMIN_USERNAME)
+node ./createAdmin.js "$ADMIN_USERNAME:$ADMIN_PASSWORD"
